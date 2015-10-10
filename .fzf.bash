ac172abbffa5e2e256b64e71361c2831add059b8
Andrew Wong <wongadev@gmail.com>
2015-10-10 13:42:35 +1100

    Add fzf shell extensions

---
+++ .fzf.bash
@@ -0,0 +1,20 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" =~ "/usr/local/Cellar/fzf/0.10.5/bin" ]]; then
+  export PATH="$PATH:/usr/local/Cellar/fzf/0.10.5/bin"
+fi
+
+# Man path
+# --------
+if [[ ! "$MANPATH" =~ "/usr/local/Cellar/fzf/0.10.5/man" && -d "/usr/local/Cellar/fzf/0.10.5/man" ]]; then
+  export MANPATH="$MANPATH:/usr/local/Cellar/fzf/0.10.5/man"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- =~ i ]] && source "/usr/local/Cellar/fzf/0.10.5/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/usr/local/Cellar/fzf/0.10.5/shell/key-bindings.bash"
+
