2801483dd447fb1e1ca008ddda41088c0a7c5471
Andrew Wong <wongadev@gmail.com>
2016-02-04 03:05:48 +1100

    homestead: update laravel installer and configure xdebug

--- vagrant/homestead/after.sh
+++ vagrant/homestead/after.sh
@@ -6,21 +6,25 @@
 
 # Set the timezone
 timedatectl set-timezone 'Australia/Sydney'
-
 sed -i "s/^;\?date.timezone =.*/date.timezone = \"Australia\/Sydney\"/" \
     $(find /etc/php -name php.ini)
 
-# Update Laravel Installer
-sudo -H -u vagrant sh -c 'composer global require laravel/installer'
-
-# Add aliases for common commands
-cat > /home/vagrant/.bash_aliases <<EOF
-# Shortcuts to common artisan commands (Laravel)
-alias pa='php artisan'
-alias pat='php artisan tinker'
+# Configure Xdebug
+[ ! -d "/vagrant/tmp" ] && mkdir -p "/vagrant/tmp"
+cat <<EOF >> /etc/php/mods-available/xdebug.ini
+xdebug.remote_enable = 1
+xdebug.remote_connect_back = 1
+xdebug.cli_color = 1
+xdebug.show_local_vars = 1
+xdebug.var_display_max_depth = 5
+xdebug.var_display_max_children = 256
+xdebug.var_display_max_data = 1024
+xdebug.profiler_enable_trigger = 1
+xdebug.profiler_output_dir = /vagrant/tmp
 EOF
-
-chown vagrant:vagrant /home/vagrant/.bash_aliases
 
 # Restart PHP
 service php7.0-fpm restart
+
+# Update Laravel Installer
+sudo -H -u vagrant sh -c 'composer global require laravel/installer'
