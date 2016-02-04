f82a2c122bc57d3689929b3debcac31cd8a76c85
Andrew Wong <wongadev@gmail.com>
2016-02-05 06:19:16 +1100

    homestead: Update settings + fix CLI PHP crashing
    - Map folder to /vagrant instead of /home/vagrant/www.
    
    - Add vi editing mode for editline and readline CLI apps.
    
    - Add alias for clearing cache and regenerating ide helper files.
    
    - Disable xdebug.show_local_vars to prevent CLI PHP crashing on errors.

--- vagrant/homestead/after.sh
+++ vagrant/homestead/after.sh
@@ -6,16 +6,16 @@
 
 # Set the timezone
 timedatectl set-timezone 'Australia/Sydney'
-sed -i "s/^;\?date.timezone =.*/date.timezone = \"Australia\/Sydney\"/" \
-    $(find /etc/php -name php.ini)
+sed -i 's/^;\?date.timezone =.*/date.timezone = "Australia\/Sydney"/' \
+    /etc/php/7.0/fpm/php.ini \
+    /etc/php/7.0/cli/php.ini
 
 # Configure Xdebug
 [ ! -d "/vagrant/tmp" ] && mkdir -p "/vagrant/tmp"
-cat <<EOF >> /etc/php/mods-available/xdebug.ini
+cat <<'EOF' >> /etc/php/mods-available/xdebug.ini
 xdebug.remote_enable = 1
 xdebug.remote_connect_back = 1
 xdebug.cli_color = 1
-xdebug.show_local_vars = 1
 xdebug.var_display_max_depth = 5
 xdebug.var_display_max_children = 256
 xdebug.var_display_max_data = 1024
@@ -28,3 +28,30 @@ service php7.0-fpm restart
 
 # Update Laravel Installer
 sudo -H -u vagrant sh -c 'composer global require laravel/installer'
+
+# Use vi editing mode for readline and editline libraries.
+cat <<'EOF' | sudo -u vagrant tee /home/vagrant/.inputrc
+$include /etc/inputrc
+set editing-mode vi
+set completion-ignore-case On
+
+$if mode=vi
+  set keymap vi-command
+  #
+  set keymap vi-insert
+  "\C-a": beginning-of-line
+  "\C-e": end-of-line
+  "\C-l": clear-screen
+  "\C-n": history-search-forward
+  "\C-p": history-search-backward
+$endif
+EOF
+
+cat <<'EOF' | sudo -u vagrant tee /home/vagrant/.editrc
+bind -v
+bind "^A" ed-move-to-beg
+bind "^E" ed-move-to-end
+bind "^L" ed-clear-screen
+bind "^N" ed-search-next-history
+bind "^P" ed-search-prev-history
+EOF
