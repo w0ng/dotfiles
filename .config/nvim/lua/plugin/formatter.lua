0d00f3953b3090af025666430e2735fa37625e56
Andrew Wong <andrew.w@canva.com>
2021-10-23 00:30:53 +1100

    nvim: use standard dprint in formatter plugin

--- .config/nvim/lua/plugin/formatter.lua
+++ .config/nvim/lua/plugin/formatter.lua
@@ -1,36 +1,12 @@
 local formatter = require('formatter')
 
 -- brew install dprint
--- local dprint_exe = 'dprint'
--- local dprint_config = '~/.config/dprint/dprint.json'
-local dprint_exe = vim.fn.getenv('WORK_DPRINT_EXE')
-local dprint_config = vim.fn.getenv('WORK_DPRINT_CONFIG')
-
-local filetype = {}
-
-if
-  vim.fn.empty(dprint_exe) == 0
-  and vim.fn.empty(dprint_config) == 0
-  and vim.fn.executable(dprint_exe) == 1
-  and vim.fn.filereadable(vim.fn.expand(dprint_config)) == 1
-then
-  for ft, ext in pairs({
-    javascript = 'js',
-    javascriptreact = 'jsx',
-    json = 'json',
-    jsonc = 'jsonc',
-    markdown = 'md',
-    typescript = 'ts',
-    typescriptreact = 'tsx',
-  }) do
-    filetype[ft] = {
-      function()
-        return {
-          exe = dprint_exe,
-          args = { 'fmt', '--config', dprint_config, '--stdin', ext },
-          stdin = true,
-        }
-      end,
+local dprint = function(ext)
+  return function()
+    return {
+      exe = 'dprint',
+      args = { 'fmt', '--config', '~/.config/dprint/dprint.json', '--stdin', ext },
+      stdin = true,
     }
   end
 end
@@ -38,12 +14,18 @@ end
 -- Add filetypes to formatter config
 formatter.setup({
   logging = false,
-  filetype = filetype,
+  filetype = {
+    javascript = { dprint('js') },
+    javascriptreact = { dprint('jsx') },
+    json = { dprint('json') },
+    jsonc = { dprint('jsonc') },
+    markdown = { dprint('md') },
+    typescript = { dprint('ts') },
+    typescriptreact = { dprint('tsx') },
+  },
 })
 
 local map = vim.api.nvim_set_keymap
--- :ww to save file without formatting
-map('c', 'ww', 'noautocmd w', { noremap = true })
 -- auto format <current filetype>
 map('n', '<Leader>af', ':Format<CR>', { noremap = true })
 map('v', '<Leader>af', ':Format<CR>', { noremap = true })
