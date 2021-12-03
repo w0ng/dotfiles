c0ebc7466baaf234fe8b8445184b6877db0613cc
Andrew Wong <andrew.w@canva.com>
2021-12-03 19:06:27 +1100

    nvim: fix diagnostic sign for 0.6

--- .config/nvim/lua/plugin/lspconfig/init.lua
+++ .config/nvim/lua/plugin/lspconfig/init.lua
@@ -124,11 +124,11 @@ vim.lsp.handlers['textDocument/implementation'] = location_handler
 -- (default { Error = 'E', Warning = 'W', Hint = 'H', Information = 'I' })
 for type, icon in pairs({
   Error = ' ',
-  Warning = ' ',
+  Warn = ' ',
   Hint = ' ',
-  Information = ' ',
+  Info = ' ',
 }) do
-  local hl = 'LspDiagnosticsSign' .. type
+  local hl = 'DiagnosticSign' .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = '' })
 end
 
