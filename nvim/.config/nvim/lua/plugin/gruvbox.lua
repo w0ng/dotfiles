8dce54bcd881cc4ea887062eea3f398f43600812
Andrew Wong <andrew.w@canva.com>
2023-10-09 21:33:32 +1100

    nvim: gruvbox: migrate to latest config

--- nvim/.config/nvim/lua/plugin/gruvbox.lua
+++ nvim/.config/nvim/lua/plugin/gruvbox.lua
@@ -1,9 +1,10 @@
-local colors = require('gruvbox.palette').colors
+local colors = require('gruvbox').palette
 
 require('gruvbox').setup({
   bold = false,
   italic = {
     strings = false,
+    emphasis = false,
     comments = false,
     operators = false,
     folds = false,
@@ -11,13 +12,13 @@ require('gruvbox').setup({
   overrides = {
     -- Transparent SignColumn
     SignColumn = { bg = colors.dark0 },
-    GruvboxRedSign = { fg = colors.red, bg = colors.dark0, reverse = false },
-    GruvboxGreenSign = { fg = colors.green, bg = colors.dark0, reverse = false },
-    GruvboxYellowSign = { fg = colors.yellow, bg = colors.dark0, reverse = false },
-    GruvboxBlueSign = { fg = colors.blue, bg = colors.dark0, reverse = false },
-    GruvboxPurpleSign = { fg = colors.purple, bg = colors.dark0, reverse = false },
-    GruvboxAquaSign = { fg = colors.aqua, bg = colors.dark0, reverse = false },
-    GruvboxOrangeSign = { fg = colors.orange, bg = colors.dark0, reverse = false },
+    GruvboxRedSign = { fg = colors.neutral_red, bg = colors.dark0, reverse = false },
+    GruvboxGreenSign = { fg = colors.neutral_green, bg = colors.dark0, reverse = false },
+    GruvboxYellowSign = { fg = colors.neutral_yellow, bg = colors.dark0, reverse = false },
+    GruvboxBlueSign = { fg = colors.neutral_blue, bg = colors.dark0, reverse = false },
+    GruvboxPurpleSign = { fg = colors.neutral_purple, bg = colors.dark0, reverse = false },
+    GruvboxAquaSign = { fg = colors.neutral_aqua, bg = colors.dark0, reverse = false },
+    GruvboxOrangeSign = { fg = colors.neutral_orange, bg = colors.dark0, reverse = false },
 
     -- Darker StatusLine
     StatusLine = { fg = colors.dark0_hard, bg = colors.bright_yellow, reverse = true },
