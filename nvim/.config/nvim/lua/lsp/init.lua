51b88a004654ce5466b321fc3ee83db6260f7b55
Andrew Wong <andrew.w@canva.com>
2025-11-13 14:57:10 +1100

    nvim: add keybinding for :LspEslintFixAll

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -27,6 +27,7 @@ vim.opt.signcolumn = 'yes'
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<LocalLeader>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '<LocalLeader>l', ':LspEslintFixAll<CR>', opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 
